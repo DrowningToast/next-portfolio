b26fe5e00f2cf4c7262e859ea6e4b82cd22ca56e
Supratouch <supratouchgussii@gmail.com>
2022-04-03 00:38:27 +0700

    Add | Test google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,20 @@ import type { AppProps } from "next/app";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 library.add(fas);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_TAG_ID)
+      return console.log("Missing Google Tag ID");
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_TAG_ID,
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
