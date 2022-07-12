b50d27a23619355ca1d6ca78569b14bfb57cf65a
Supratouch <supratouchgussii@gmail.com>
2022-07-12 16:41:56 +0700

    fix | attempting to fix cache controlling

--- next.config.js
+++ next.config.js
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png|jpeg|gltf|glb)",
-        locale: false,
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=9999999999, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
+  headers: async () => [
+    {
+      // list more extensions here if needed; these are all the resources in the `public` folder including the subfolders
+      source: "/:all*(svg|jpg|png|gltf|glb)",
+      locale: false,
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=31536000, stale-while-revalidate",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
