3fd2d40c70d1e8d318afb7f299591d633a3359cd
Supratouch <supratouchgussii@gmail.com>
2022-08-13 01:48:28 +0700

    Refactor Contact component

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,13 +1,23 @@
 import ReactTypingEffect from "react-typing-effect";
 import ContactScene from "@components/r3f/Scenes/Contact";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Link from "next/link";
+import { useInView } from "framer-motion";
 
 const Contact = () => {
   const [selected, setSelected] = useState<null | string>(null);
 
+  const target = useRef<HTMLDivElement>(null);
+  const inView = useInView(target, {
+    once: true,
+    margin: "0px 360px",
+  });
+
   return (
-    <section className="h-screen text-white w-full lg:px-12 px-4 lg:py-20 pt-10 pb-0 flex justify-start lg:flex-row flex-col items-center z-20 relative">
+    <section
+      ref={target}
+      className="h-screen text-white w-full lg:px-12 px-4 lg:py-20 pt-10 pb-0 flex justify-start lg:flex-row flex-col items-center z-20 relative"
+    >
       <div className="absolute inset-0 flex flex-row-reverse">
         <div className="lg:w-1/2 w-full h-full relative flex flex-col-reverse">
           <div className="w-full lg:h-1/2 h-1/6 flex justify-center gap-x-24 items-center text-xl underline underline-offset-2">
@@ -56,7 +66,7 @@ const Contact = () => {
         </h2>
       </div>
       <div className="lg:w-1/2 w-full lg:h-full h-1/2 relative">
-        <ContactScene selected={selected} />
+        {inView && <ContactScene selected={selected} />}
       </div>
     </section>
   );
